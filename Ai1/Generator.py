b4ef1fef5b5b7a5e5bab4314f8f0e3c88c46f222
Machado <elitonmachadod200@gmail.com>
2020-02-09 11:43:24 -0300

    P5.py - slow

--- Ai1/Generator.py
+++ Ai1/Generator.py
@@ -1,13 +1,18 @@
-from random import randint
+from math import *
+from random import *
 
 from Ai1.Dna import Dna
 
 
 def rand_char():
-    return chr(randint(32, 126))
+    c = randint(97, 123)
+    if c == 123:
+        c = 32
+    return chr(c)
 
 
 class Generator:
+    gen_count = 0
     pop = []
     max_pop = 0
     mutation_rate = 0
@@ -20,6 +25,7 @@ class Generator:
         self.generate_random_pop()
 
     def generate_random_pop(self):
+        self.gen_count = 0
         for i in range(self.max_pop):
             s = ""
             for j in range(len(self.target)):
@@ -38,6 +44,33 @@ class Generator:
     def calc_fitness(self):
         for d in self.pop:
             d.fitness = 0
+
             for i in range(len(self.target)):
                 if d.s[i] == self.target[i]:
                     d.fitness += 1
+            d.fitness /= len(self.target)
+
+    def next_gen(self):
+        self.gen_count += 1
+        pool = []
+
+        for i in range(len(self.pop)):
+            for j in range(floor(self.pop[i].fitness * 100)):
+                pool.append(i)
+        # print(len(pool))
+
+        new_pop = []
+        for i in range(self.max_pop):
+            p1 = self.pop[pool[randint(0, len(pool) - 1)]]
+            p2 = self.pop[pool[randint(0, len(pool) - 1)]]
+
+            s = ""
+            for j in range(len(self.target)):
+                if random() < self.mutation_rate:
+                    s += rand_char()
+                else:
+                    r = randint(0, 1)
+                    s += p1.s[j] if r == 0 else p2.s[j]
+            new_pop.append(Dna(s))
+
+        self.pop = new_pop
--- Ai1/P5.py
+++ Ai1/P5.py
@@ -1,44 +1,77 @@
 from p5 import *
+import time
 
 from Ai1.Generator import Generator
 
+TEXT_SIZE = 16
+HEIGHT = 1366
+WIDTH = 768
+
+flag = False
 generator = None
-font = None
+st, ft = 0, 0
 
 
 def setup():
-    global generator, font
-    size(1366, 768)
+    global generator
+    size(HEIGHT, WIDTH)
 
-    max_pop = 10
+    max_pop = 40
     mutation_rate = 0.1
-    target = "Uma moeda é uma moeda independente cuso o qual esteja virada"
+    target = "uma moeda e uma moeda independente do lado que esteja virada"
+    # target = "banana"
 
     generator = Generator(max_pop, mutation_rate, target)
-
-    font = create_font("Georgia", 16)
-    generator.print_pop()
+    helvetica = create_font("/home/axe/Documents/GitHub/Ai1/helvetica.otf", TEXT_SIZE)
+    text_font(helvetica)
+    # generator.print_pop()
 
 
 def draw():
-    background(255)
+    global flag, st, ft
+    if mouse_is_pressed:
+        flag = True
+        st = time.time()
+    if flag:
 
-    global generator
-    generator.calc_fitness()
+        background(255)
+        global generator
 
-    display()
+        generator.calc_fitness()
+
+        display()
+
+        if generator.check():
+            ft = time.time()
+            print("FOI")
+            print(ft - st)
+            no_loop()
+            return
+        else:
+            generator.next_gen()
 
 
 def display():
-    global generator, font
+    global generator
     med_fit = 0
     for d in generator.pop:
         med_fit += d.fitness
-    med_fit / len(generator.pop)
+    med_fit /= len(generator.pop)
 
     fill(0)
-    # text_font(font, 16)
-    text(str(med_fit), (10, 200))
+    text("Generation: %d" % generator.gen_count, (10, 200))
+    text("Fitness média: %.5f" % med_fit, (10, 200 + TEXT_SIZE))
+
+    x = 200
+    y = 10
+    for d in generator.pop:
+        text(d.s, (x, y))
+        text("%.2f" % d.fitness, (x + 10 + text_width(d.s), y))
+        y += TEXT_SIZE
+
+        if y + TEXT_SIZE >= HEIGHT:
+            y = 10
+            x += text_width(generator.target) + 100
 
 
 run()
